56c5e87a1fc28ae45224ac4d7eb8a1124528a84b
tiennguyen5184 <134986557+tiennguyen5184@users.noreply.github.com>
2024-09-30 21:35:50 +0700

    2nd version. Finish CI  (#3)
    * Create iam policy for ec2
    
    * update iam policy for public ecr
    
    * 30_9_2024 9h32am
    
    * CI version 30_9_24 2h25pm

--- CI/ec2.tf
+++ CI/ec2.tf
@@ -1,9 +1,12 @@
+data "aws_caller_identity" "current" {}
 data "aws_ssm_parameter" "ubuntu_ami" {
   name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
-
+locals {
+    account_id = data.aws_caller_identity.current.account_id
+}
 provider "aws" {
-  region =  "us-east-1" # Default region 
+  region = "us-east-1" # Default region 
 }
 
 resource "aws_security_group" "ci_aws_security_group" {
@@ -43,12 +46,13 @@ resource "aws_security_group" "ci_aws_security_group" {
 }
 
 resource "aws_instance" "ci-server" {
-  ami             = data.aws_ssm_parameter.ubuntu_ami.value
-  instance_type   = "t2.micro" # Change this to your desired instance type
-  security_groups = [aws_security_group.ci_aws_security_group.name]
-  user_data       = "${file("${path.module}/docker-install.sh")}"
+  ami                  = data.aws_ssm_parameter.ubuntu_ami.value
+  instance_type        = "t2.micro" # Change this to your desired instance type
+  security_groups      = [aws_security_group.ci_aws_security_group.name]
+  user_data            = file("${path.module}/docker-install.sh")
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   root_block_device {
-    volume_size = 30  # Size in GB
+    volume_size = 30 # Size in GB
   }
 
   tags = {
@@ -56,3 +60,60 @@ resource "aws_instance" "ci-server" {
   }
 }
 
+resource "aws_iam_policy" "ec2_policy" {
+  name = "ec2_policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr-public:CreateRepository",
+                "ecr-public:UploadLayerPart",
+                "ecr-public:BatchDeleteImage",
+                "ecr-public:DeleteRepository",
+                "ecr-public:PutImage",
+                "ecr-public:CompleteLayerUpload",
+                "ecr-public:InitiateLayerUpload",
+                "ecr-public:DeleteRepositoryPolicy",
+                "ecr-public:GetAuthorizationToken",
+                "ecr-public:BatchCheckLayerAvailability"
+            ],
+            "Resource": "arn:aws:ecr-public::${local.account_id}:repository/*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr-public:GetAuthorizationToken",
+                "ecr-public:DescribeRegistries",
+                "sts:GetServiceBearerToken"
+            ],
+            "Resource": "*"
+        }
+    ],
+  })
+}
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
+resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+            
+    ]
+  })
+}
